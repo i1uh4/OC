d248c1526b73eef6330d9782148acc864db433e0
i1uh4 <105651496+i1uh4@users.noreply.github.com>
2022-05-23 17:59:40 +0300

    Add files via upload

--- cpu.sh
+++ cpu.sh
@@ -1,5 +1,5 @@
 cat /proc/cpuinfo| grep 'model name'| uniq
-hwinfo | grep 'Units/Processor'| uniq
+cat /proc/cpuinfo | grep "cpu cores" | uniq
 hwinfo | grep 'Clock'| uniq
 lscpu | grep 'L1d cache'
 lscpu | grep 'L1i cache'
