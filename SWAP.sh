68259f8786e117a682e43d079d47da0478a6c169
i1uh4 <105651496+i1uh4@users.noreply.github.com>
2022-05-26 18:09:01 +0300

    Add files via upload

--- SWAP.sh
+++ SWAP.sh
@@ -1,7 +1,9 @@
+echo "Input the swap file size in gigabytes without any letters "
+read swapsize
 
 sudo swapoff -a /swapfile
 sudo rm -f /swapfile
-sudo fallocate -l 4G /swapfile
+sudo fallocate -l ${swapsize}G /swapfile
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
 sudo swapon /swapfile
