dfee32e90104ce330d78a3909eaafdf64ca41251
i1uh4 <105651496+i1uh4@users.noreply.github.com>
2022-05-28 11:12:46 +0300

    Add files via upload

--- Do_File_Exist.sh
+++ Do_File_Exist.sh
@@ -1,15 +1,15 @@
 echo "Input the directory "
 read DIR
-echo "Input the path to the file without expansion"
-read PTH
 
 if [ -e /${DIR} ];
 then
-	echo "Directory exists "
+	echo -e "Directory exists \n"
+	echo "Input the name of the file "
+	read PTH
 	if [ -e /${DIR}/${PTH} ];
 	then
-		file -f /${DIR}/${PTH}
-		echo "The path to the file is /${DIR}/${PTH}"
+		file -b /${DIR}/${PTH}
+		echo "The path to the file or directory is /${DIR}/${PTH}"
 	else
 		echo "File doesn't exist"
 	fi
